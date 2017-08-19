ebc430e2e386d2f6ba9f3469c1a2b39001ceced6
purvanshi <purumehta69@gmail.com>
2017-08-19 16:37:03 +0530

    fixed bugs

--- model.py
+++ model.py
@@ -6,16 +6,11 @@ from keras.layers.embeddings import Embedding
 from keras.layers import Dense, Merge, Dropout, RepeatVector
 from keras.layers import recurrent
 from keras.models import Sequential
-from keras.models import model_from_json
 from keras.preprocessing.sequence import pad_sequences
 from collections import OrderedDict
-import numpy as np
+from keras.models import model_from_json
 import h5py
-from nltk.tokenize import sent_tokenize
-from nltk.tokenize import word_tokenize
-
-
-
+import numpy as np
 np.random.seed(1337)  # for reproducibility
 
 # from keras.utils.data_utils import get_file
@@ -39,16 +34,11 @@ def parse_stories(lines, only_supporting=False):
     story = []
     for line in lines:
         nid, line = line.split(' ', 1)
-        #print("line aboive"+str(line))
         nid = int(nid)
         if nid == 1:
             story = []
         if '\t' in line:
-            #print("Line"+str(line))
             q, a, supporting = line.split('\t')
-            #print("q"+str(q))
-            #print("a"+str(a))
-            #print("supporting"+str(supporting))
             q = tokenize(q)
             substory = None
             if only_supporting:
@@ -75,7 +65,7 @@ def get_stories(f, only_supporting=False, max_length=None):
     flatten = lambda data: reduce(lambda x, y: x + y, data, [])
     data = [(flatten(story), q, answer) for story, q,
             answer in data if not max_length or len(flatten(story)) < max_length]
-    return data    
+    return data
 
 def vectorize_stories(data, word_idx, word_idx_answer, story_maxlen, query_maxlen):
     X = []
@@ -84,6 +74,7 @@ def vectorize_stories(data, word_idx, word_idx_answer, story_maxlen, query_maxle
     for story, query, answer in data:
         x = [word_idx[w] for w in story]
         xq = [word_idx[w] for w in query]
+        # let's not forget that index 0 is reserved
         y = np.zeros(len(word_idx_answer))
         for item in answer.split():
             if re.search('\+|\-|\*|/', item):
@@ -93,51 +84,20 @@ def vectorize_stories(data, word_idx, word_idx_answer, story_maxlen, query_maxle
         Y.append(y)
     return pad_sequences(X, maxlen=story_maxlen), pad_sequences(Xq, maxlen=query_maxlen), np.array(Y)
 
-def vectorize(story,query,word_idx,word_idx_answer,story_maxlen, query_maxlen):
-    print(query)
-    X=[]
-    XQ=[]
-    x = [word_idx[w] for w in story]
-    X.append(x)
-    xq = [word_idx[w] for w in query]
-    XQ.append(xq)
-    a=pad_sequences(X,maxlen=story_maxlen)
-    b=pad_sequences(XQ,maxlen=query_maxlen)
-    return pad_sequences(X,maxlen=story_maxlen),pad_sequences(XQ,maxlen=query_maxlen)
+def serialising():
+    xmodel_json = model.to_json()
+    with open("model.json","w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("model.h5")
+    print("Saved model to disk")
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
 
-def chunck_question(question):
-    '''Takes out question part from the whole question
-    '''
-    list_q = sent_tokenize(question)
-    question_word=["How","When","What","Find","Calculate"]
-    for i in range(len(list_q)):
-        for j in range(len(question_word)):
-            print(list_q[i],question_word[j])
-            if question_word[j] in list_q[i]:
-                query = list_q[i]
-                del list_q[i]
-                break
-    return list_q,query
-
-def find_answer(operation,numlist):
-    num1=numlist[0]
-    num2=numlist[1]
-    if operation=='+':
-        return num1+num2
-    elif operation=='-':
-        p=num1-num2
-        if(p>0):
-            return p
-        else:
-            return (p*-1)
-    elif operation=='*':
-        return num1*num2
-    else:
-        q=num1/num2
-        if(q>1):
-            return q
-        else:
-            return num2/num1
 
 
 RNN = recurrent.LSTM
@@ -148,28 +108,14 @@ BATCH_SIZE = 32
 EPOCHS = 40
 print('RNN / Embed / Sent / Query = {}, {}, {}, {}'.format(RNN,
                                                            EMBED_HIDDEN_SIZE, SENT_HIDDEN_SIZE, QUERY_HIDDEN_SIZE))
+
 train = get_stories(open(sys.argv[1], 'r'))
 test = get_stories(open(sys.argv[2], 'r'))
-question="Jane had 4 apples. She gave 1 to Umesh. How many apples does jane hav now?"
-story,query=chunck_question(question)
-print(story)
-new_story=[]
-new_query=[]
-for i in story:
-    x=word_tokenize(i)
-    for j in x:
-        new_story.append(str(j))
 
-new_query=word_tokenize(query)
-n_query=list(map(str,new_query))
 
 vocab = sorted(reduce(lambda x, y: x | y,
                       (set(story + q + [answer]) for story, q, answer in train + test)))
-for i in n_query:
-    vocab.append(i)
-for i in new_story:
-    vocab.append(i)
-    
+# Reserve 0 for masking via pad_sequences
 vocab_size = len(vocab) + 1
 vocab_answer_set = set()
 for story, q, answer in train + test:
@@ -181,15 +127,18 @@ vocab_answer_size = len(vocab_answer)
 word_idx = OrderedDict((c, i + 1) for i, c in enumerate(vocab))
 word_idx_answer = OrderedDict((c, i) for i, c in enumerate(vocab_answer))
 word_idx_operator_reverse = OrderedDict((i, c) for i, c in enumerate(vocab_answer))
+print('a', word_idx_answer)
 story_maxlen = max(map(len, (x for x, _, _ in train + test)))
 query_maxlen = max(map(len, (x for _, x, _ in train + test)))
 
 X, Xq, Y = vectorize_stories(train, word_idx, word_idx_answer, story_maxlen, query_maxlen)
 tX, tXq, tY = vectorize_stories(test, word_idx, word_idx_answer, story_maxlen, query_maxlen)
 
-
-print("erer"+str(n_query))
-xp,xqp=vectorize(new_story,n_query,word_idx,word_idx_answer,story_maxlen,query_maxlen)
+# print('vocab = {}'.format(vocab))
+print('X.shape = {}'.format(X.shape))
+print('Xq.shape = {}'.format(Xq.shape))
+print('Y.shape = {}'.format(Y.shape))
+print('story_maxlen, query_maxlen = {}, {}'.format(story_maxlen, query_maxlen))
 
 print('Build model...')
 print(vocab_size, vocab_answer_size)
@@ -211,20 +160,30 @@ model.add(RNN(EMBED_HIDDEN_SIZE, return_sequences=False))
 model.add(Dropout(0.3))
 model.add(Dense(vocab_answer_size, activation='softmax'))
 
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-loaded_model.compile(optimizer='adam',
+model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
+
+print('Training')
+
+model.fit([X, Xq], Y, batch_size=BATCH_SIZE,
+          nb_epoch=EPOCHS, validation_split=0.05)
+
+
+loss, acc = model.evaluate([tX, tXq], tY, batch_size=BATCH_SIZE)
+print("Testing")
+print('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
+loss, acc = model.evaluate([X, Xq], Y, batch_size=BATCH_SIZE)
+print("Training evaluation")
+print('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
 goldLabels = list()
 predictedLabels = list()
-for pr in loaded_model.predict([xp, xqp]):
-    predictedLabels.append(word_idx_operator_reverse[np.argsort(pr)[-1]])
-print(predictedLabels)
-numlist=list(int(s) for s in question.split() if s.isdigit())
-answer=find_answer(predictedLabels[0],numlist)
-print(answer)
+# for y in tY:
+#     sortedLabels = np.argsort(y)
+#     goldLabels.append(word_idx_operator_reverse[sortedLabels[-1]])
+# print('True Labels', 'Predictions')
+# for pr in model.predict([tX, tXq]):
+#     predictedLabels.append(word_idx_operator_reverse[np.argsort(pr)[-1]])
+# print(goldLabels, predictedLabels)
+# print('\n'.join(goldLabels), '\n'.join(predictedLabels))
+# print('\n'.join(list(map(lambda x: x[0] + ', ' + x[1], list(zip(goldLabels, predictedLabels))))))
